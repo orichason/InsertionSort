2c9dccec6219119a3cad0608d67dc1fca9e11f0b
orichason <orichason@gmail.com>
2022-07-21 13:38:51 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void InsertionSort(List<int> UnsortedList)
+        {
+            int temp;
+
+            for (int i = 1; i < UnsortedList.Count; i++)
+            {
+                if (UnsortedList[i] > UnsortedList[i - 1])
+                {
+                    for (int y = i; i < 0; y++)
+                    {
+                        if(UnsortedList[i] < UnsortedList[y])
+                        {
+                            temp = UnsortedList[i];
+                            UnsortedList[y] = temp;
+                            UnsortedList[i] = UnsortedList[y];
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> List = new List<int> { 4, 9, 1, 43, 22, 10 };
+
+            InsertionSort(List);
+        }
+    }
+}
